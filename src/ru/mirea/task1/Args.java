50968c296a85f06600aeb6060f44b1168cb7687b
Boldyrev Alex <113009764+PudgeCyclone@users.noreply.github.com>
2023-05-14 14:39:43 +0300

    Create Args.java

---
+++ src/ru/mirea/task1/Args.java
@@ -0,0 +1,12 @@
+package ru.mirea.task1.opt3;
+
+import java.util.Scanner;
+
+public class Args {
+    public static void main(String[] args) {
+        for (int i = 0; i < args.length; i++) {
+            System.out.print(args[i] + " ");
+        }
+
+    }
+}
